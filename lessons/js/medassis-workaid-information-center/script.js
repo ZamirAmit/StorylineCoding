cd14cc70d5e1fb720e74480cbefc4273bc1141c1
ZamirAmit <zamir.amit@gmail.com>
2023-04-11 17:55:18 +0300

    add

--- lessons/js/medassis-workaid-information-center/script.js
+++ lessons/js/medassis-workaid-information-center/script.js
@@ -20,3 +20,59 @@ $(document).ready(function() {
     });
 
 });
+// This function contain
+function setDial(startNumber, endNumber) {
+
+    endNumberIsRealNumber = isNaN(endNumber);
+    endNumberIsPositive = endNumber <= 0;
+    if (endNumberIsRealNumber || endNumberIsPositive) {
+        return 'There was a problem with the inputS types'
+    }
+
+    let dialAnimmation = setInterval(runDial, 50);
+    /*
+        The dialAnimmation method will:
+        1. execute the function runDial until the end of it.
+        2. Wait 50 mili-seconds
+        3. run it again
+       * Only ClearInterval method will stop it *
+   */
+
+    // This function moving th dial
+    function runDial() {
+
+        isDialinished = startNumber + 1 == endNumber;
+        isDialNegetive = endNumber <= 0;
+        if (isDialinished || isDialNegetive) {
+
+            clearInterval(dialAnimmation); // This method stops the dial
+
+        }
+        // Adding one value startNumber
+        startNumber++;
+        // Passing the value into storyline scope
+        Player.SetVar("dial", startNumber);
+        // This is will whow on the actual dial
+
+    } // End of function
+
+
+}
+let Player = GetPlayer();
+try {
+    /* This Block of code will try
+        will try to excute itselft
+        and will jump to 'catch' block
+        if an error has ocured
+    */
+
+    setDial(0, 100);
+
+} catch (err) {
+
+    console.log(err.name);
+    console.log(err.message);
+    console.log(err.stack);
+
+
+}
