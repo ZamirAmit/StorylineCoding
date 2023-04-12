5dfa31f14741199b61c17cba97e3f70eedd1d9bd
ZamirAmit <zamir.amit@gmail.com>
2023-04-12 13:44:00 +0300

    update with fetch

--- lessons/js/fetchAndJQUERY/story_content/user.js
+++ lessons/js/fetchAndJQUERY/story_content/user.js
@@ -2,6 +2,36 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5nGr3InZTRI":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  
+let player = GetPlayer();
+let file = "story_content/external_files/questions.txt";
+var headers = new Headers();
+headers.append('Content-Type', 'text/plain; charset=UTF-8');
+fetch(file, headers)
+
+
+    .then(function (content) {
+        return content.text();
+    })
+    .then(function (text) {
+        rows = text.split('\n');
+        question = rows[1].split(',');
+        
+        player.SetVar('questionTitle', question[0]);
+        player.SetVar('correctAnswer', question[1]);
+        player.SetVar('firstDistractor', question[2]);
+        player.SetVar('secondDistractor', question[3]);
+        player.SetVar('successFeedback', question[4]);
+        player.SetVar('failFeedback', question[5]);
+
+    });
 }
 
