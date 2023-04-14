e8766446ee7a9809af1e8c3241bcee739adaf21e
ZamirAmit <zamir.amit@gmail.com>
2023-04-14 15:41:42 +0300

    add loop

--- lessons/js/dashboard/script.js
+++ lessons/js/dashboard/script.js
@@ -12,12 +12,45 @@ async function fetchDataFromApi() {
 
 }
 
-function finish(joke) {
-    console.log(joke);
-    console.log('Finished fetching data');
+function getDataFromJson(json) {
+    data = json.data;
+    // All this variables have to be global:
+    let dashboardObject = {
+        totalGrades: 0,
+        totalTime: 0,
+        passAmount: 0,
+        failAmount: 0
+
+    }
+    for (_line of data) {
+        // += is short for: totalGrades = totalGrades +_line.StudentGrade;
+        dashboardObject.totalGrades += _line.StudentGrade;
+        dashboardObject.totalTime += _line.StudentTotalTime;
+
+        // Check if score has Pass status:
+        if (_line.GradeStatus == 'Pass') {
+            dashboardObject.passAmount++; // Add to pass
+        } else {
+            dashboardObject.failAmount++; // Add to fail
+        }
+    }
+
+    // Average  = sum of group / amount of group.
+    averageGrade = Math.round(totalGrades / studentsAmount);
+    averageTime = totalTime / studentsAmount;
+    // Converting from mili-seconds to seconds:
+    averageTimeInSeconds = averageTime / 1000;
+    // Converting from seconds to minutes:
+    averageTimeInMinutes = Math.floor(averageTimeInSeconds / 60);
+    // Caculating remainig seconds after subtracting total from seconds:
+    remainAverageSecond = Math.round(averageTimeInSeconds - averageTimeInMinutes * 60);
+    // Formatting the time to readable version:
+    dashboardObject.formatedAverageTime = averageTimeInMinutes + ":" + remainAverageSecond;
+    return dashboardObject;
 }
 async function init() {
-    let joke = await fetchDataFromApi();
-    finish(joke);
+    let json = await fetchDataFromApi();
+    data = json.data;
+    console.log(getDataFromJson(json));
 }
 init();
